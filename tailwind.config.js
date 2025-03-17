d6ee0a53137127dc7cdb3bbb6d640538afb5be71
jhojax12866 <jhojanfer12@gmail.com>
2025-03-17 14:02:47 -0500

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 module.exports = {
@@ -23,29 +22,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-/** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    colors: {
-      ...colors,
-      primary: colors.blue,
-      secondary: colors.blue,
-    },
-  },
-  plugins: [],
-};
->>>>>>> 5d4d35e (add new files)
