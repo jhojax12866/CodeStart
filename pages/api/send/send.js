a210ecb340143d1b46f32940f5d3f85d790912ea
jhojax12866 <jhojanfer12@gmail.com>
2025-04-11 13:31:46 -0500

    example

--- pages/api/send/send.js
+++ pages/api/send/send.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-      user: "johanburbano2021@itp.edu.co",  // CAMBIA ESTO
+      user: "johanburbano2021@itp.edu.co",  
       pass: "1124848674",         // CAMBIA ESTO (Usa una contraseña de aplicación en Gmail)
     },
   });
